70534254a7db5d9d97c0dc8d05971cceb0362106
Damian <m4gn3s@gmail.com>
2024-01-16 13:05:29 +0100

    kk

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cafes.db"
+db = SQLAlchemy()
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    sql_query = text("SELECT * FROM cafe")
+    result = db.session.execute(sql_query)
+    all_cafes = result.fetchall()
+    return render_template('index.html', cafes=all_cafes)
 
 if __name__=="__main__":
     app.run(debug=True)
